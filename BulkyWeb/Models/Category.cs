6c4a391af666e5b521b483d7b179d42bd607fad5
GokhanTuncer <gokhan_alphan@hotmail.com>
2024-04-29 00:37:26 +0300

    B

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,8 +8,12 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-        public int DisplayOrder { get; set; }
+		[DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage="Aralık 1-100 arası olmalıdır")]
+		public int DisplayOrder { get; set; }
 
     }
 }
